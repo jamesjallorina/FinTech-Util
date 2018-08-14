7127dcf1b7d91343130e3fe4ff23829040bcb328
Jallorina <James.Jallorina@FISGLOBAL.COM>
2018-08-14 16:24:01 +0800

    improve logging message

--- debug/libsrc/debug.cpp
+++ debug/libsrc/debug.cpp
@@ -29,6 +29,7 @@ Abstract:
 
 #include <ctime>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <iostream>
 #include <ofstream>
 #include "debug.hpp"
@@ -36,21 +37,38 @@ Abstract:
 
 using namespace std;
 
-#define DBG_LEN 2024
 #define APP_NAME_LEN 50
 
 static const char progname[APP_NAME_LEN + 1];
 static const char const *FileName;
 
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function needs to be initialized first before you can use logging utility  	*
+*				  This initialize application name													*
+*****************************************************************************************************/
 int app_name_init(char *app_name)
 {
+	static bool isInitAppName = false;
 	if(progname[0]) 
 		return (-1);
 	else 
+	{
 		strcpy(progname, app_name);
+		isInitAppName = true;
+	}
 	return (0);
 }
 
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function needs to be initialized first before you can use logging utility  	*
+*				  This initialize file name															*
+*****************************************************************************************************/
 int debugOn(char *filename)
 {
 	static bool isInit = false;
@@ -67,95 +85,161 @@ int debugOn(char *filename)
 	return (0);
 }
 
-int printInfo(char *printmsg)
-{
-	char debug[DBG_LEN +1];
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function prints debug message with level info												  	*
+*****************************************************************************************************/
+int printInfo(const char *format, ...)
+{	
+	char debug[2024+1];
+	char printmsg[1024+1];
+	va_list arg;
+	
 	ofstream fout(FileName);
-	fout.open(FileName);
+	fout.open(FileName, ios::out|ios::app);
 
 	if(isFileOk(fout) == false) return (-1);
 	
 	memset(debug, '\0', sizeof(debug));
+	memset(printmsg, '\0', sizeof(debug));
+	
 	time_t curr_time = time(0);
 	tm *local_time = localtime(&curr_time);
 	
-	
-	sprintf("%02d:%02d:%04d %02d:%02d:%02d [   %s]: INFO: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
+	va_start(arg, format);
+	sprintf(printmsg, format);
+	va_end(arg);
+	sprintf(debug, "%02d:%02d:%04d %02d:%02d:%02d [   %s]: INFO: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
 	fout << debug;
 	fout.close();
 	return (0);
 }
 
-	
-	
-	
-int printDebug(char *printmsg)
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function prints debug message with level debug							  	*
+*****************************************************************************************************/
+int printDebug(char *printmsg, const char *format, ...)
 {
-	char debug[DBG_LEN +1];
-	ofstream fout(FileName);
-	fout.open(FileName);
+	char debug[2024+1];
+	char printmsg[1024+1];
+	va_list arg;
 	
+	ofstream fout(FileName);
+	fout.open(FileName, ios::out|ios::app);
+
 	if(isFileOk(fout) == false) return (-1);
 	
 	memset(debug, '\0', sizeof(debug));
+	memset(printmsg, '\0', sizeof(debug));
+	
 	time_t curr_time = time(0);
 	tm *local_time = localtime(&curr_time);
 	
-	sprintf("%02d:%02d:%04d %02d:%02d:%02d [   %s]: DEBUG: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
+	va_start(arg, format);
+	sprintf(printmsg, format);
+	va_end(arg);
+	sprintf(debug, "%02d:%02d:%04d %02d:%02d:%02d [   %s]: DEBUG: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
 	fout << debug;
 	fout.close();
 	return (0);
 }
 
-int printWarn(char *printmsg)
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function prints debug message with level warning							  	*
+*****************************************************************************************************/
+int printWarn(char *printmsg, const char *format, ...)
 {
-	char debug[DBG_LEN +1];
-	ofstream fout(FileName);
-	fout.open(FileName);
+	char debug[2024+1];
+	char printmsg[1024+1];
+	va_list arg;
 	
+	ofstream fout(FileName);
+	fout.open(FileName, ios::out|ios::app);
+
 	if(isFileOk(fout) == false) return (-1);
 	
 	memset(debug, '\0', sizeof(debug));
+	memset(printmsg, '\0', sizeof(debug));
+	
 	time_t curr_time = time(0);
 	tm *local_time = localtime(&curr_time);
 	
-	sprintf("%02d:%02d:%04d %02d:%02d:%02d [   %s]: WARN: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
+	va_start(arg, format);
+	sprintf(printmsg, format);
+	va_end(arg);
+	sprintf(debug, "%02d:%02d:%04d %02d:%02d:%02d [   %s]: WARN: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
 	fout << debug;
 	fout.close();
 	return (0);
 }
 
-int printFatal(char *printmsg)
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function prints debug message with level fatal							  	*
+*****************************************************************************************************/
+int printFatal(char *printmsg, const char *format, ...)
 {
-	char debug[DBG_LEN +1];
-	ofstream fout(FileName);
-	fout.open(FileName);
+	char debug[2024+1];
+	char printmsg[1024+1];
+	va_list arg;
 	
+	ofstream fout(FileName);
+	fout.open(FileName, ios::out|ios::app);
+
 	if(isFileOk(fout) == false) return (-1);
 	
 	memset(debug, '\0', sizeof(debug));
+	memset(printmsg, '\0', sizeof(debug));
+	
 	time_t curr_time = time(0);
 	tm *local_time = localtime(&curr_time);
 	
-	sprintf("%02d:%02d:%04d %02d:%02d:%02d [   %s]: FATAL: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
+	va_start(arg, format);
+	sprintf(printmsg, format);
+	va_end(arg);
+	sprintf(debug, "%02d:%02d:%04d %02d:%02d:%02d [   %s]: FATAL: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
 	fout << debug;
 	fout.close();
 	return (0);
 }
 
-int printError(char *printmsg)
+/****************************************************************************************************
+*	Author		: James Marjun Jallorina												          	*
+*	Date		: 08-14-2018                                                                      	*
+*	Parameter	: char * app_name /application name                                               	*
+*	Description	: This function prints debug message with level error							  	*
+*****************************************************************************************************/
+int printError(char *printmsg, const char *format, ...)
 {
-	char debug[DBG_LEN +1];
-	ofstream fout(FileName);
-	fout.open(FileName);
+	char debug[2024+1];
+	char printmsg[1024+1];
+	va_list arg;
 	
+	ofstream fout(FileName);
+	fout.open(FileName, ios::out|ios::app);
+
 	if(isFileOk(fout) == false) return (-1);
 	
 	memset(debug, '\0', sizeof(debug));
+	memset(printmsg, '\0', sizeof(debug));
+	
 	time_t curr_time = time(0);
 	tm *local_time = localtime(&curr_time);
 	
-	sprintf("%02d:%02d:%04d %02d:%02d:%02d [   %s]: ERROR: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
+	va_start(arg, format);
+	sprintf(printmsg, format);
+	va_end(arg);
+	sprintf(debug, "%02d:%02d:%04d %02d:%02d:%02d [   %s]: ERROR: %s \n", 1 +local_time->tm_mon, local_time->tm_mday, 1970 + local_time->tm_year, 1 + local_time->tm_hour, 1 + local_time->tm_min, 1 + local_time->tm_sec, progname, printmsg);
 	fout << debug;
 	fout.close();
 	return (0);
