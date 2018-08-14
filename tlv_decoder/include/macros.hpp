05b6ae5784e779d5e4bbdc136aca9b8fe1a76d07
Jallorina <James.Jallorina@FISGLOBAL.COM>
2018-08-14 17:22:28 +0800

    fixed error on macros

--- tlv_decoder/include/macros.hpp
+++ tlv_decoder/include/macros.hpp
@@ -30,9 +30,9 @@ Abstract:
 #ifndef _MACRO_H_
 #define _MACRO_H_
 
-#define init_char(str) memset(str,'\0',sizeof(str)
-#define isFileOk(file)	if(!file.is_open()) { cout << "failed to open file!\n"; return (-1) } \
-		else return (0)
+#define init_char(str) (memset(str,'\0',sizeof(str)))
+#define isFileOk(file)	(!file.is_open() ? 1 : 0)
+#define isInitializedDebug(a,b)	((a && b) ? 1 : 0)
 	
 
 #endif	//_MACRO_H_
